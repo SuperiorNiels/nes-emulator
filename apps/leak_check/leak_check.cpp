ad4816862d8ccae90ec99840667bb19011aa8012
Niels Balemans <niels.balemans@uantwerpen.be>
2021-04-26 22:02:38 +0200

    added leak test app

---
+++ apps/leak_check/leak_check.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "Memory.h"
+#include "SDL/Window_SDL.h"
+
+const char* WINDOW_TITLE = "Leak Check (Memory)";
+const int WINDOW_WIDTH = 500;
+const int WINDOW_HEIGHT = 50;
+
+
+int main(int argc, char* args[])
+{
+    for(int i = 0; i < 20; i++) {
+        auto mem = Memory(1000000000); // 5 Gb mem
+        mem.reset();
+    }
+
+    auto factory = WindowFactory_SDL();
+    auto window = factory.createWindow(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    window->init();
+    while(window->isOpen()) window->update();
+    window->close();
+
+
+    return 0;
+}
